eb2a74d0e2bb967bbee92fe74f8ecc27ff4485e0
teegoood <teegooodgood@gmail.com>
2025-04-09 10:35:05 +0700

    try fix swagger

--- api/index.js
+++ api/index.js
@@ -4,18 +4,28 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
+const CSS_URL =
+	'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
 
 // Load swagger.json
 const swaggerDocument = JSON.parse(
-  fs.readFileSync(path.join(__dirname, '../docs', 'swagger.json'), 'utf8')
+	fs.readFileSync(path.join(__dirname, '../docs', 'swagger.json'), 'utf8')
 );
 
 // Serve Swagger UI
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(
+	'/',
+	swaggerUi.serve,
+	swaggerUi.setup(swaggerDocument, {
+		customCss:
+			'.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+		customCssUrl: CSS_URL,
+	})
+);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Swagger UI hosted at http://localhost:${PORT}/api-docs`);
+	console.log(`Swagger UI hosted at http://localhost:${PORT}`);
 });
 
 module.exports = app;
