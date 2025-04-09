5e893475e9fd31b29ec6d35cd47150175af48fff
teegoood <teegooodgood@gmail.com>
2025-04-09 10:22:51 +0700

    fix veercel

--- api/index.js
+++ api/index.js
@@ -7,13 +7,15 @@ const app = express();
 
 // Load swagger.json
 const swaggerDocument = JSON.parse(
-  fs.readFileSync(path.join(__dirname, 'docs', 'swagger.json'), 'utf8')
+  fs.readFileSync(path.join(__dirname, '../docs', 'swagger.json'), 'utf8')
 );
 
 // Serve Swagger UI
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Swagger UI hosted at http://localhost:${PORT}/api-docs`);
 });
+
+module.exports = app;
