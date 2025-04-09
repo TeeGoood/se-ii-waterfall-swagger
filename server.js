ff5f85099f43b1555ba761b8dd6d72202e594a82
teegoood <teegooodgood@gmail.com>
2025-04-09 10:07:21 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+// Load swagger.json
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.join(__dirname, 'docs', 'swagger.json'), 'utf8')
+);
+
+// Serve Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Swagger UI hosted at http://localhost:${PORT}/api-docs`);
+});
